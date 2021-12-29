6cdc7d103ec85dacf5905cc3de56ef366e0614fb
ymcla <ymclapp@yahoo.com>
2021-12-29 14:25:53 -0600

    setup app.js and app is running for terms

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [termsAccepted, setTermsAccepted] = useState(false);
+  if (!termsAccepted) {
+    return (
+      <>
+      <h1>Terms of Service</h1>
+      <p>These are the terms for using the application.</p>
+      <button
+      onClick={() => {
+        setTermsAccepted(true);
+      }}
+      >
+        I accept
+      </button>
+      </>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>This is the application</div>
   );
 }
 
