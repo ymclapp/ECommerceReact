10fa0b1f77ad69e856625370e3234e37d7492cdb
ymcla <ymclapp@yahoo.com>
2021-12-29 14:35:54 -0600

    updated app.js to get an item from session storage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function getSessionStorageOrDefault(key, defaultValue) {
+  const stored = sessionStorage.getItem(key);
+  if(!stored) {
+    return defaultValue;
+  }
+  return JSON.parse(stored);
+}
 
 function App() {
 
-  const [termsAccepted, setTermsAccepted] = useState(false);
+  const [termsAccepted, setTermsAccepted] = useState(
+    getSessionStorageOrDefault('terms',false)
+  );
+
+  
   if (!termsAccepted) {
     return (
       <>
