f5fda4d17dfbc6c0ba91dc7431f157b404b9d4a5
ymcla <ymclapp@yahoo.com>
2021-12-30 11:00:28 -0600

    cleaned up app.js for commented out items

--- src/App.js
+++ src/App.js
@@ -1,46 +1,10 @@
 import React from 'react';
 import './App.css';
-//import { useSessionStorage } from './hooks/useSessionStorage';
 import { BrowserRouter as Switch, Route } from 'react-router-dom';
 import Registration from './components/Users/Registration';
 import Home from './components/Home';
 
-//moved to useSessionStorage.js
-//function getSessionStorageOrDefault(key, defaultValue) {
-// const stored = sessionStorage.getItem(key);
-//  if(!stored) {
-//   return defaultValue;
-// }
-// return JSON.parse(stored);
-//}
-
 function App() {
-
-  //const [termsAccepted, setTermsAccepted] = useState(
-  //  useSessionStorage('terms', false)
- // );
-
-  //moved to useSessionStorage.js
-  // useEffect(() => {
-  //   sessionStorage.setItem('terms', JSON.stringify(termsAccepted));
-  // }, [termsAccepted]);
-
-//moved to Home.js
-//  if (!termsAccepted) {
-//    return (
-//      <>
-//        <h1>Terms of Service</h1>
-//        <p>These are the terms for using the application.</p>
- //       <button
- //         onClick={() => {
- //           setTermsAccepted(true);
- //         }}
- //       >
- //         I accept
- //       </button>
- //     </>
- //   );
-  //}
 
   return (
     <>
